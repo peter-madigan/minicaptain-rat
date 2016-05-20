2ad8e56ce6b9851d9c406b041a409281055c5374
Peter Madigan <pmadigan@pdsf11.nersc.gov>
2016-05-20 16:36:16 -0700

    2016-05-20

--- git.sh
+++ git.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-
+echo "Performing $1..."
+if [ "$1" == "push" ]; then
 # Pushes current version to github
-
-git add *.sh
-git add *.ratdb
-git add *.geo
-git add *.md
-git add mac/*.mac
-git add mac/*.loop
-
-DATE=`date +"%Y-%m-%d"`
-git commit -m "${DATE}"
-
-git push -u origin master
+    git add *.sh
+    git add *.ratdb
+    git add *.geo
+    git add *.md
+    git add mac/*.mac
+    git add mac/*.loop
+    
+    DATE=`date +"%Y-%m-%d"`
+    git commit -m "${DATE}"
+    
+    git push -u origin master
+elif [ "$1" == "pull" ]; then
+# Gets current version from github    
+    git fetch origin master
+    git pull
+else
+    echo "ERROR - Option $1 not recognized!"
+fi
