dd00a4df44613da8451adaecd05dc87e53983143
Peter Madigan <pmadigan@pdsf11.nersc.gov>
2016-05-25 16:04:02 -0700

    2016-05-25

--- scripts/runSingle.sh
+++ scripts/runSingle.sh
@@ -5,6 +5,6 @@ module avail
 cd /global/homes/p/pmadigan/work/sim
 source setup.sh
 source rat-pac/env.sh
-cd minicaptain-rat
+cd minicaptain-rat/logs
 
-rat -o "data/minicaptain_demo/minicaptain_demo_${runnumber}.root" -s${runnumber} mac/minicaptain_demo.mac
+rat -o "../data/minicaptain_demo/minicaptain_demo_${runnumber}.root" -s${runnumber} ../mac/minicaptain_demo.mac
